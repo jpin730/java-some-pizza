752b921e1ac629447bbefce64cbd5a3b91f713e1
Jaime Pineda <jpin730@pm.me>
2024-04-01 12:38:06 -0600

    feat: :tada: initial commit

---
+++ src/main/java/com/example/somepizza/SomePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.somepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomePizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomePizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/somepizza/SomePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomePizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
