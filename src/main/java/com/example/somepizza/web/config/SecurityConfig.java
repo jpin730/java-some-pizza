ef7fd154bb5fc2011b94cc61fbbbd9119c71eaba
Jaime Pineda <jpin730@pm.me>
2024-04-04 16:40:47 -0600

    feat: :passport_control: add customer and admin users in memory

--- src/main/java/com/example/somepizza/web/config/SecurityConfig.java
+++ src/main/java/com/example/somepizza/web/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.example.somepizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +19,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                        authorize -> authorize.anyRequest().permitAll()).
+                        authorize -> authorize.
+                                requestMatchers(HttpMethod.GET, "/reset").hasRole("ADMIN").
+                                requestMatchers(HttpMethod.GET, "/**").hasAnyRole("ADMIN", "CUSTOMER").
+                                requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN").
+                                requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN").
+                                requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")).
                 csrf(
                         AbstractHttpConfigurer::disable
                 ).cors(Customizer.withDefaults()).
                 httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+        UserDetails customer = User
+                .withUsername("customer")
+                .password(passwordEncoder().encode("password"))
+                .roles("CUSTOMER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
